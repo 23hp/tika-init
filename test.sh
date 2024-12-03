06a949d818f5256d7d82acf2e499f2aa8121c65e
Albert Hu <albert.hu@staffbase.com>
2024-12-03 12:35:51 +0100

    add wait

--- test.sh
+++ test.sh
@@ -7,7 +7,8 @@ printf "\n=== Sending a job to Tika\n"
 curl  -d '{ "id":"test","fetcher":"s3f", "fetchKey": "README.md", "emitter":"s3e"}' \
       -X POST http://localhost:9998/pipes
 
-printf "\n=== Checking results\n"
+printf "\n=== Checking results after emitWithinMillis\n"
+sleep 3
 curl  --fail  "http://localhost:9090/tika-results/README.md.json" || { docker logs tika ;exit 1; }
 
 printf "\n=== Tika works!"
