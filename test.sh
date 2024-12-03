35473a1aa3250453f67b502598364f43a411d7d6
Albert Hu <albert.hu@staffbase.com>
2024-12-03 12:25:51 +0100

    use different endpoint

--- test.sh
+++ test.sh
@@ -4,11 +4,10 @@ printf "=== Uploading a file to the fetcher bucket"
 curl --request PUT --upload-file ./README.md http://localhost:9090/files/README.md
 
 printf "\n=== Sending a job to Tika\n"
-curl  -d '[{ "id":"test","fetcher":"s3f", "fetchKey": "README.md", "emitter":"s3e"}]' \
-      -X POST http://localhost:9998/async
+curl  -d '{ "id":"test","fetcher":"s3f", "fetchKey": "README.md", "emitter":"s3e"}' \
+      -X POST http://localhost:9998/pipes
 
-printf "\n=== Checking the result\n"
-sleep 3
+printf "\n=== Checking results\n"
 curl  --fail  "http://localhost:9090/tika-results/README.md.json" || { printf "Tika doesn't work"; exit 1; }
 
 printf "\n=== Tika works!"
