1b45faf76ad612c5e0dc3ca284aa6be5d016b541
Albert Hu <albert.hu@staffbase.com>
2024-12-03 12:32:30 +0100

    print err when fails

--- test.sh
+++ test.sh
@@ -8,6 +8,6 @@ curl  -d '{ "id":"test","fetcher":"s3f", "fetchKey": "README.md", "emitter":"s3e
       -X POST http://localhost:9998/pipes
 
 printf "\n=== Checking results\n"
-curl  --fail  "http://localhost:9090/tika-results/README.md.json" || { printf "Tika doesn't work"; exit 1; }
+curl  --fail  "http://localhost:9090/tika-results/README.md.json" || { docker logs tika ;exit 1; }
 
 printf "\n=== Tika works!"
