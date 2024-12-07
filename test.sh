8d1422f7af25c76fb722b52020840e9dba67ac19
Albert Hu <albert.hu@staffbase.com>
2024-12-07 13:29:04 +0100

    refine test

--- test.sh
+++ test.sh
@@ -4,16 +4,18 @@ printf "=== Uploading a file to the fetcher bucket"
 curl --request PUT --upload-file ./README.md http://localhost:9090/files/README.md
 
 printf "\n=== Sending a job to Tika\n"
-curl  -d '{ "id":"test","fetcher":"s3f", "fetchKey": "README.md", "emitter":"fse"}' \
+curl  -d '{ "id":"test","fetcher":"s3f", "fetchKey": "README.md", "emitter":"fse","emitKey":"test"}' \
       -X POST http://localhost:9998/pipes
 
 printf "\n=== Checking results after emitWithinMillis\n"
 sleep 2
 
-if [ ! -f ./output/README.md.json ]; then
+if [ ! -f ./tika-output/test.json ]; then
     printf "File not found!\n"
     docker logs tika
     exit 1
 fi
+
+rm ./tika-output/test.json
 
 printf "\n=== Tika works!"
