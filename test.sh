b0934c7096a154dac66e5d746295a86fa1725cf3
Albert Hu <albert.hu@staffbase.com>
2024-12-07 15:26:53 +0100

    rollback

--- test.sh
+++ test.sh
@@ -5,4 +5,14 @@ curl --request PUT --upload-file ./README.md http://localhost:9090/files/README.
 
 printf "\n=== Sending a job to Tika\n"
 curl  -d '{ "id":"test","fetcher":"s3f", "fetchKey": "README.md", "emitter":"fse","emitKey":"test"}' \
-      -X POST --silent --fail http://localhost:9998/pipes || docker logs tika; exit 1
+      -X POST  http://localhost:9998/pipes
+
+if [ ! -f ./tika-output/test.json ]; then
+    printf "File not found!\n"
+    docker logs tika
+    exit 1
+fi
+
+rm ./tika-output/test.json
+
+printf "\n=== Tika works!"
